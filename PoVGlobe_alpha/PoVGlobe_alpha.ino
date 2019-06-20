53048c2a400ea3dd62b391cb0f6806bee22f2861
EventHor1zon <3v3nt_h0r1z0n@protonmail.com>
2019-06-20 20:50:25 +0100

    Add files via upload

--- PoVGlobe_alpha/PoVGlobe_alpha.ino
+++ PoVGlobe_alpha/PoVGlobe_alpha.ino
@@ -19,7 +19,11 @@
  *        made python program to turn .ppm files into the correct image type
  *        made test images & tested them
  *        
- *         STILL have problems with the START/END protocol of LEDs. First one always white with 'end', dark with 'start'
+ *        [x] Still have problems with the START/END protocol of LEDs. First one always white with 'end', dark with 'start' ::: FIXED
+ *         [+]Error was in the endedness of the data shifting.
+ *        [x] Need to solve how to write LEDs as a single strip
+ *          [+] Sorted with offset and a reverse array reading function, quite trivial
+ *        [-] Problem with image converting script needs to swap blue/red pixel values
  *          
  * 
  */
@@ -31,24 +35,29 @@
 
 #include <Arduino.h>
 
-#include "image7.h"
+#include "image5.h"
 
 #define NUMLEDS     9
 #define T_MAX       0xFFFF
 
 
+/************* Function Prototypes *****************/
 
 void spi_start();
 void spi_end();
 void spi_write(uint8_t *x);
 void write_frame(uint32_t *data);
+void write_frame_fwd(uint32_t *data);
+void write_frame_rev(uint32_t *data);
 
-bool GO = 0;
-bool SPI_W = 0;
+
+/************* VARIABLES ****************************/
+
 uint8_t LED_START = 0x00;
 uint8_t LED_END = 0xFF;
 uint8_t GLOBAL = 0xE0;
 uint8_t BRIGHTNESS = 0x1F;
+
 uint32_t T_NOW = 0;
 uint32_t T_LAST = 0;
 uint32_t T_ROT = 0;
@@ -56,11 +65,12 @@ uint16_t FRAME_T=0;
 uint16_t FRAME_C=0;
 uint16_t FRAME_NO = 126;
 uint16_t TEMP =0;
-uint16_t img_size = FRAME_NO * NUMLEDS * 4;
+int frame_size = (NUMLEDS * sizeof(uint32_t));
 void *image_ptr;
 
 
 uint32_t data[NUMLEDS];
+uint32_t OFFSET = NUMLEDS * FRAME_NO;
 
 /***************   INTERRUPT FUNCTIONS *********************/
 
@@ -75,12 +85,7 @@ ISR(INT0_vect){                           //  External Interrupt Vector on PORTD
     } else { T_ROT = T_NOW - T_LAST; }    // otherwise t_rot = t_now - t_last
     T_LAST=T_NOW;                         // set t_last for next use
     FRAME_T = T_ROT/(FRAME_NO/2);         // get frame_t - triggers twice so divide frame_no by 2
-    if(FRAME_T < T_MAX) { 
-        GO = 1;
-      }
-    else {
-        GO = 0;
-      }
+
     SREG=sreg;                            // restore interrupt settings
     PIND = (1 << PIND3);
   }                                       // end interrupt
@@ -91,7 +96,7 @@ ISR(TIM1_COMPA_vect){                     // interrupt on timer match; i.e FRAME
   cli();                                  // clear interrupts
   FRAME_C++;                              // advance the frame counter
   if (FRAME_C > FRAME_NO) FRAME_C = 0;    // if frames completed, reset frame_c
-  SPI_W=1;                                // set SPI write flag - don't write here, too CPU intensive
+//  SPI_W=1;                                // set SPI write flag - don't write here, too CPU intensive
   TEMP=TCNT1+FRAME_T;                     // find value for next interrupt. In case of overflow, use temp buffer
   if(TEMP > T_MAX){                         // if higher than counter max value
     OCR1A = (uint16_t)(TEMP-T_MAX);         // next frame is t - max temp variable? May need to typecast from a larger val)
@@ -99,46 +104,72 @@ ISR(TIM1_COMPA_vect){                     // interrupt on timer match; i.e FRAME
   SREG=sreg;                              // restore interrupts
 }
 
+/****************** SPI FUNCTIONS **********************/
 
 void spi_write(uint8_t *x){                // SPI write function ( uses uint8_t as 8-byte val)
     SPDR = *x;                             // pass x into the 8 bit spi data reg
     while ((SPSR & (1<<SPIF)) == 0);      // wait until SPI transaction concluded (compare SPI reg with SPI done flag)
+    return;
 }
 
 
 void spi_start(){               // start frame 32 * 0 bits
   for(int i=0; i<4; i++){       // send 4 lots of START
-    spi_write(&LED_START);           // (start is 0)
+    SPDR = 0x00;                             // pass x into the 8 bit spi data reg
+    while ((SPSR & (1<<SPIF)) == 0);      // wait until SPI transaction concluded (compare SPI reg with SPI done flag)
   }
 }
 
 void spi_end(){                 // end frame 32 * 1 bits
-  for(int i=0; i<32; i++){       // send 4 lots of END - try more - see note
-      spi_write(&LED_END);             // end = 1
+  for(int i=0; i<2; i++){       // send 4 lots of END - try more - see note
+    SPDR = 0xFF;                             // pass x into the 8 bit spi data reg
+    while ((SPSR & (1<<SPIF)) == 0);      // wait until SPI transaction concluded (compare SPI reg with SPI done flag)
   }
 }
 
-void write_frame(uint32_t *frame_ptr){                                        // takes pointer to data structure
+void write_frames(uint32_t *frame_ptr){                                        // takes pointer to data structure
                                                                               // if using progmem to hold data must retrieve with special functions first.
-    memcpy_P((void *)&data, (const void *)frame_ptr, sizeof(uint32_t)* 9);    // retrieve frame data from progmemory
-    uint8_t *ptr=(uint8_t *)data;                                             // a uint8_t pointer to address in data
+    //memcpy_P((void *)&data, (const void *)frame_ptr, sizeof(uint32_t)* 9);    // retrieve frame data from progmemory
+    //uint8_t *ptr=(uint8_t *)&data[0];                                             // a uint8_t pointer to address in data
     spi_start();                                                              // write startframe
-    for(int i=0; i<NUMLEDS; i++){                                                             // for each LED
-    spi_write(ptr);                        // write global
-    spi_write(ptr+1);                   // next 8 bits for red
-    spi_write(ptr+2);                // next 8 bits for green
-    spi_write(ptr+3);                // next 8 for blue
-    ptr += 4;                        //  uint8_t POINTERS MOVE IN size of uint8_ts! NOT BITS, FOO!!
-    }
-    spi_end();                                      // send end frame
+    write_frame_rev(frame_ptr);                                               // write 1st half of globe
+    write_frame_fwd(frame_ptr+OFFSET);                                    // write 2nd half of globe -reverso- style
+    spi_end();                                                                // send end frame
 }
 
+void write_frame_rev(uint32_t *frame_ptr){
+    
+    memcpy_P((void *)&data, (const void *)frame_ptr, sizeof(uint32_t) * NUMLEDS);
+    uint8_t *ptr=(uint8_t *)&data[NUMLEDS-1];                                // for the 1st frame, write bottom to top
+    for(int i=0; i<NUMLEDS; i++){ 
+        spi_write(ptr+3);                                                    // write global
+        spi_write(ptr+2);                                                    // next 8 bits for blue
+        spi_write(ptr+1);                                                    // next 8 bits for green
+        spi_write(ptr);                                                      // next 8 for red
+        ptr -= 4;                                                            //  move backwards through the array
+      }
+  }
+
+void write_frame_fwd(uint32_t *frame_ptr){
+
+    memcpy_P((void *)&data, (const void *)frame_ptr, sizeof(uint32_t) * NUMLEDS);
+    uint8_t *ptr=(uint8_t *)&data[0];                                        // for the 2nd frame, write top to bottom
+    for(int i=0; i<NUMLEDS; i++){ 
+        spi_write(ptr+3);                                                    // write global
+        spi_write(ptr+2);                                                    // next 8 bits for blue
+        spi_write(ptr+1);                                                    // next 8 bits for green
+        spi_write(ptr);                                                      // next 8 for red
+        ptr += 4;                                                            //  move forwards through the array 
+      }
+  }
+
+/********************* INIT FUNCTIONS ****************************/
 
 void initSPI(){                           // sets up SPI interface
     PRR = (0 << PRSPI);                     // turns SPI on in Power Reduction registers
     DDRB = (1<<DDB2)|(1<<DDB3)|(1<<DDB5);     // init PortB pins as output - SS, MOSI, SCK
     SPCR = (1<<SPE)|(1<<MSTR)|(0<<SPR1)|(0<<SPR0);  // spi_enable | spi_master mode | spi speed setting
-    SPSR = (1<<SPI2X);                      // set double speed on spi
+    SPSR = (0<<SPI2X);                      // set double speed on spi
     PORTB &= ~(1<<PB2);                     // set SS pin low - LEDs are dumb (technical term) but still need SS low in order to write out
 }
 
@@ -158,7 +189,7 @@ void initGPIO(){
     // interrupt on falling edge of signal
 
     DDRD = (0 << DDD2) | (1 << DDD3);         // set d2 as input, d3 as output
-    PORTD = (0 << PORTD0) | (1 << PORTD3);      // disable pullup resistor, put d3 out high
+    PORTD = (1 << PORTD2) | (0 << PORTD3);      // disable pullup resistor, put d3 out high
     EIMSK = (1 << INT0);                      // enable interrupt mask
     EICRA = (1 << ISC01) | (1 << ISC00);    // select  rising edge int
     EIFR = (1 << INTF0);                    // enable interrupt routine flag
@@ -171,7 +202,6 @@ void setup() {
   initGPIO();
   //initTimer1();
   initSPI();
-
   image_ptr=&image[0];
 }
 
@@ -188,21 +218,13 @@ void wait(int iters){
     }
   }
 
+
+/************ MAIN LOOP ******************/
+
 void loop() {
   // put your main code here, to run repeatedly:
-  int sz = (NUMLEDS * sizeof(uint32_t));
   
-  //Serial.println(buff);
-  for(int i=0; i<FRAME_NO; i++){
-        write_frame(image_ptr);
-        image_ptr+=sz;      // increment pointer to next frame
-  //    memset((void *)&buff, "\0", sizeof(buff));
-  //    sprintf(buff, "pointer val: %p\n", image_ptr);
-  //    Serial.println(buff);
-        delay(10);
-  }
-  image_ptr=&image[0];
-  delay(1000);
+  
   
 }
 
